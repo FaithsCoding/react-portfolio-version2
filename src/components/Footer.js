f50d7100212c42660384bb56281fa4bae6b7c6cd
Faith Meades <122907573+FaithsCoding@users.noreply.github.com>
2023-06-28 14:17:07 +0100

    used react components to  create footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,8 @@
 import { Container, Row, Col } from "react-bootstrap";
-import logo from "../assets/img/logo.svg";
 import navIcon1 from "../assets/img/nav-icon1.svg";
 import navIcon2 from "../assets/img/nav-icon2.svg";
 import navIcon3 from "../assets/img/nav-icon3.svg";
+import Resume from "./Resume";
 
 export const Footer = () => {
   return (
@@ -10,18 +10,26 @@ export const Footer = () => {
       <Container>
         <Row className="align-items-center">
           <Col size={12} sm={6}>
-            <img src={logo} alt="Logo" />
+            <Resume />
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
-              <a href="#"><img src={navIcon1} alt="Icon" /></a>
-              <a href="#"><img src={navIcon2} alt="Icon" /></a>
-              <a href="#"><img src={navIcon3} alt="Icon" /></a>
+              <a href="https://www.linkedin.com/in/faith-meades-a66b20276/">
+                <img src={navIcon1} alt="" />
+              </a>
+              <a href="https://github.com/FaithsCoding">
+                <img src={navIcon2} alt="" />
+              </a>
+              <a href="https://www.instagram.com/designerrprojects/?hl=en-gb">
+                <img src={navIcon3} alt="" />
+              </a>
             </div>
-            <p>Copyright 2022. All Rights Reserved</p>
+            <p>
+              Made with love by FaithsCoding 2023.
+            </p>
           </Col>
         </Row>
       </Container>
     </footer>
-  )
-}
+  );
+};
