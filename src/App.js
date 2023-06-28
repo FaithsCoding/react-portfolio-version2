f08bf4d7b5b47153bd697372a429dbeaaeb3b207
Faith Meades <122907573+FaithsCoding@users.noreply.github.com>
2023-06-28 14:43:39 +0100

    added all images, redefined contact in app.js. updated package file with dependencies

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { NavBar } from "./components/NavBar";
 import { Banner } from "./components/Banner";
 import { AboutMe } from "./components/AboutMe";
 import { Projects } from "./components/Projects";
-import { Contact } from "./components/Contact";
+import Contact  from "./components/Contact";
 import  Resume  from "./components/Resume";
 import { Footer } from "./components/Footer";
 
