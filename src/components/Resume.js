3ec33fa7fdbf5c8fd9d454dc1cde89c2c475ff75
Faith Meades <122907573+FaithsCoding@users.noreply.github.com>
2023-06-28 14:18:04 +0100

    added details into resume and made the pdf downloadable

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,21 +1,31 @@
 import React from "react";
-import { Container, Button } from "react-bootstrap";
+import { Container } from "react-bootstrap";
+import { ArrowRightCircle } from "react-bootstrap-icons";
 
 const Resume = () => {
-  const resumeUrl = "src/assets/resume/Faith_Meades_CV.pdf"; // Replace with the actual path to your resume PDF
-
-  const handleDownload = () => {
-    window.open(resumeUrl, "_blank");
-  };
+  const resumeUrl = "/assets/resume/Faith_Meades_CV.pdf";
 
   return (
-    <section id="resume">
+    <section className="resume">
       <Container>
-        <h2>Resume</h2>
-        <p>
-          Download my resume to learn more about my skills, experience, and qualifications.
-        </p>
-        <Button variant="primary" onClick={handleDownload}>Download Resume</Button>
+        <div className="resume-section">
+          <h2>Resume</h2>
+          <p>
+            I am efficient in HTML, CSS, JavaScript, Web Api's, Third Party
+            Api's, Server-Side Api's, Node, Object-Orientated Programming,
+            Express, SQL, Object-Relational Mapping, NoSQL, Progressive Web
+            Applications, and React. Download my resume to learn more about my
+            skills, experience, and qualifications.
+          </p>
+          <a href={resumeUrl} download>
+            <button
+              className="resume-button"
+              style={{ backgroundColor: "black", color: "white" }}
+            >
+              Download Resume <ArrowRightCircle size={20} />
+            </button>
+          </a>
+        </div>
       </Container>
     </section>
   );
